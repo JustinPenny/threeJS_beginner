c6b12fa951cfec79212a6498299f86b89ed874f2
Justin Penny <justincpenny@hotmail.com>
2025-10-23 23:42:40 -0400

    working house model added. working on fine touches

--- src/main.js
+++ src/main.js
@@ -1,37 +1,55 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-// playing with threejs and orbit controls. Want to make something neat to display or portfolio. Currently have a rotateable cube.
 
-//https://threejs.org/docs/#examples/en/controls/OrbitControls
+// Add progress bar loader and instructions for mouse controls
 
+// Setup and creation of display elements
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-//controls = new THREE.OrbitControls( camera, renderer.domElement );
-
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const materials = [
-    new THREE.MeshBasicMaterial({ color: 0xff0000 }), // Right face (+x)
-    new THREE.MeshBasicMaterial({ color: 0x00ff00 }), // Left face (-x)
-    new THREE.MeshBasicMaterial({ color: 0x0000ff }), // Top face (+y)
-    new THREE.MeshBasicMaterial({ color: 0xffff00 }), // Bottom face (-y)
-    new THREE.MeshBasicMaterial({ color: 0x00ffff }), // Front face (+z)
-    new THREE.MeshBasicMaterial({ color: 0xff00ff })  // Back face (-z)
-];
+// ambient lighting
+const light = new THREE.AmbientLight( 0x404040, 15 ); // soft white light
+scene.add( light );
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const cube = new THREE.Mesh(geometry, materials);
-scene.add( cube );
+// background color, couldn't make an image work
+const color = new THREE.Color().setHex(0x87CEEB);
+scene.background = color;
 
-camera.position.z = 5;
+
+
+// Instantiate a loader
+const loader = new GLTFLoader();
+
+// Load a glTF resource
+loader.load(
+	// resource URL
+	'src/my_house.gltf',
+	function ( gltf ) {
+		scene.add( gltf.scene );
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+    let width = ( xhr.loaded / xhr.total * 100 );
+	},
+	// called when loading has errors
+	function ( error ) {
+		console.log( 'An error happened' );
+	}
+);
+
+
+camera.position.z = 2;
 
 function animate() {
   renderer.render( scene, camera );
 }
 renderer.setAnimationLoop( animate );
 
+// Rotational Controls
 const controls = new OrbitControls( camera, renderer.domElement );
 
